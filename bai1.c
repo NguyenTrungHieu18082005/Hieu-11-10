1b68fe6a570c518ba98539f07686c6a1d9bfa0f0
NguyenTrungHieu18082005 <trunghieu18082005@gmail.com>
2024-10-11 19:37:47 +0700

    Initial commit

---
+++ bai1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a, b;
+    printf("Nhap vao a:");
+    scanf("%d", &a);
+    printf("Nhap vao a:");
+    scanf("%d", &a);
+
+    printf("So a la: %d \n", a);
+    printf("So b la: %d \n", b);
+
+    printf("Toi vua su file nay:");
+
+    return 0;
+}
